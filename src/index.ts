35f5844b44818eb654e7d80cf5743edc173622bd
Yury <72691412+cheatsnake@users.noreply.github.com>
2022-01-30 21:39:48 +0300

    Use product router

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import "dotenv/config";
 import express, { Application, Request, Response } from "express";
 import cors from "cors";
 import { connection } from "./configs/database.config";
+import { productRouter } from "./routes/product.router";
 
 const PORT: number = Number(process.env.PORT) || 5000;
 const app: Application = express();
@@ -9,6 +10,7 @@ const app: Application = express();
 app.use(cors());
 app.use(express.json());
 
+app.use("/api", productRouter);
 app.get("/", (req: Request, res: Response) => {
     res.send("Server is work");
 });
