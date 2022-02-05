84e227553a476402dc9d28ea1244743a88d99e17
Yury <72691412+cheatsnake@users.noreply.github.com>
2022-02-05 22:48:26 +0300

    Update error constants

--- src/utils/error.constants.ts
+++ src/utils/error.constants.ts
@@ -1,3 +1,5 @@
 export const INCORRECT_DATA = "Incorrect data";
 export const SERVER_ERROR = "Something went wrong, please try again later";
 export const ITEM_NOT_FOUND = "Item not found";
+export const UNAUTHORIZED = "The user is not authorised";
+export const NO_ACCESS_ALLOWED = "No access allowed";
