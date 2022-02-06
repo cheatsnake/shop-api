0bf9f0704c9cffc705215854527e620f4cc27390
Yury <72691412+cheatsnake@users.noreply.github.com>
2022-02-06 11:06:27 +0300

    Add admin protection

--- src/routes/category.router.ts
+++ src/routes/category.router.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
+import { adminMiddleware } from "../middlewares/admin.middleware";
 import { CategoryController } from "../controllers/category.controller";
 
 const router = Router();
 const categoryController = new CategoryController();
 
-router.post("/category", categoryController.create);
+router.post("/category", adminMiddleware(), categoryController.create);
 router.get("/category", categoryController.findAll);
 
 export { router as categoryRouter };
--- src/routes/product.router.ts
+++ src/routes/product.router.ts
@@ -1,14 +1,15 @@
 import { ProductController } from "../controllers/product.controller";
 import { Router } from "express";
+import { adminMiddleware } from "../middlewares/admin.middleware";
 
 const router = Router();
 const productController = new ProductController();
 
-router.post("/product", productController.create);
+router.post("/product", adminMiddleware(), productController.create);
 router.get("/product/:id", productController.findById);
 router.get("/products/:category", productController.findByCategory);
 router.get("/products", productController.findAll);
-router.put("/product", productController.updateById);
-router.delete("/product/:id", productController.deleteById);
+router.put("/product", adminMiddleware(), productController.updateById);
+router.delete("/product/:id", adminMiddleware(), productController.deleteById);
 
 export { router as productRouter };
